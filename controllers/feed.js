93035b1fc8089b6de95729bad9bea0966f9cc30a
virghiu@Virjhiu <virghiu@gmail.com>
2020-05-13 18:47:39 -0400

    managment image and crud post feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
@@ -22,11 +25,18 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
+
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
-    imageUrl: 'images/duck.jpg',
+    imageUrl: imageUrl,
     content: content,
     creator: {
       name: 'Virghiu',
@@ -66,4 +76,88 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatedPost = (req, res, next) => {
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+  const imageUrl = req.body.image;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    const error = new Error('No file picked');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Post not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        deleteImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: 'Updated successfully',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.postDelete = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Post not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      deleteImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({ message: 'Deleted post' });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const deleteImage = (filePath) => {
+  filePath = path.join(__dirname, '..', 'images');
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -19,4 +19,14 @@ router.post(
 
 router.get('/post/:postId', feedController.getPost);
 
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.updatedPost
+);
+
+router.delete('/post/:postId', feedController.postDelete);
 module.exports = router;
