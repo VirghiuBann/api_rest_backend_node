726bbf5eb7a9e915c6d5ab13bb331e443b5b545a
virghiu <virghiu@gmail.com>
2020-07-09 17:27:40 -0400

    complete socket.io

--- controllers/feed.js
+++ controllers/feed.js
@@ -13,6 +13,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -105,37 +106,31 @@ exports.getPost = async (req, res, next) => {
 
 exports.updatedPost = async (req, res, next) => {
   const postId = req.params.postId;
-  const title = req.body.title;
-  const content = req.body.content;
-  const imageUrl = req.body.image;
-
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
+    const error = new Error('Validation failed, entered data is incorrect.');
     error.statusCode = 422;
     throw error;
   }
-
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
   if (req.file) {
     imageUrl = req.file.path;
   }
-
   if (!imageUrl) {
-    const error = new Error('No file picked');
+    const error = new Error('No file picked.');
     error.statusCode = 422;
     throw error;
   }
-
   try {
-    const post = await Post.findById(postId);
-
+    const post = await Post.findById(postId).populate('creator');
     if (!post) {
-      const error = new Error('Post not found.');
+      const error = new Error('Could not find post.');
       error.statusCode = 404;
       throw error;
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized!');
       error.statusCode = 403;
       throw error;
@@ -143,16 +138,12 @@ exports.updatedPost = async (req, res, next) => {
     if (imageUrl !== post.imageUrl) {
       deleteImage(post.imageUrl);
     }
-
     post.title = title;
     post.imageUrl = imageUrl;
     post.content = content;
     const result = await post.save();
-
-    res.status(201).json({
-      message: 'Updated successfully',
-      post: result,
-    });
+    io.getIO().emit('posts', { action: 'update', post: result });
+    res.status(200).json({ message: 'Post updated!', post: result });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -180,7 +171,7 @@ exports.postDelete = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.post.pull(postId);
     await user.save();
-
+    io.getIO().emit('posts', { action: 'delete', post: postId });
     res.status(200).json({ message: 'Deleted post' });
   } catch (err) {
     if (!err.statusCode) {
