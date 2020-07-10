21490131d3ce82e3e843c59a2162638e936d5f58
virghiu <virghiu@gmail.com>
2020-07-10 18:03:54 -0400

    add validation user

--- app.js
+++ app.js
@@ -56,6 +56,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    customFormatErrorFn: (err) => {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error ocurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
